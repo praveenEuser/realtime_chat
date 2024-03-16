c960470e0df04ef2fde6b6155ee42384f81de50a
praveenEuser <praveenkumarofficialacc@gmail.com>
2024-03-16 10:00:03 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 
 // // https://vitejs.dev/config/
 // export default defineConfig({
-//   server: {host:'10.10.150.183', port:5173},
 //   plugins: [react()],
 // })
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {host:'192.168.11.170', port:5173},
+  plugins: [react()],
+})
